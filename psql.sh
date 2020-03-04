bf0d32401c756397403f8ca45e095163d2ee46a4
James Tan <james_tan97@outlook.com>
2020-03-05 01:13:45 +0800

    Add -y to apt install commands

--- psql.sh
+++ psql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt update
-apt install postgresql postgresql-contrib
+apt update -y
+apt install postgresql postgresql-contrib -y
 psqlver=$(psql -V | egrep -o '[0-9]{1,}\.[0-9]{1,}')
 echo $psqlver
 echo "host    all             all           0.0.0.0/0         md5" >>  /etc/postgresql/"$psqlver"/main/pg_hba.conf
